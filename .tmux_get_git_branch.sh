00e9f55ffcfb92034364f135ecce112915a5440e
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2016-11-19 22:55:54 +0900

    Added script for check branch name

--- .tmux_get_git_branch.sh
+++ .tmux_get_git_branch.sh
@@ -1,6 +1,3 @@
-#!/bin/bash -e
-# Thank you for nice trick!
-# http://qiita.com/koara-local/items/940ce66e2ecd8e4d8582
-cd `tmux display-message -p -F "#{pane_current_path}"`
-branch_name=`git branch | grep \*.* | sed -e 's/\*\ //'`
+#!/bin/bash
+branch_name=$(git -C "#{pane_current_path}" --abbrev-ref HEAD 2>&-)
 [ ! -z ${branch_name} ] && echo "⭠ ${branch_name}"
