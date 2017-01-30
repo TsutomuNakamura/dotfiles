e371e47d2c661e9c0e1349158782e19bce34e781
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2017-01-30 18:57:21 +0900

    Fixed tmux config to print git repository name

--- .tmux_get_git_branch.sh
+++ .tmux_get_git_branch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-branch_name=$(git -C "#{pane_current_path}" --abbrev-ref HEAD 2>&-)
+#branch_name=$(git -C "#{pane_current_path}" --abbrev-ref HEAD 2>&-)
+branch_name=$(git -C $1 rev-parse --abbrev-ref HEAD 2>&-)
 [ ! -z ${branch_name} ] && echo "⭠ ${branch_name}"
