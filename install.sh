e1532f0057a0dc40b2737aa43e2598ae183a7d8f
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2018-11-03 22:08:38 +0900

    Added pushd when install you_complete_me

--- install.sh
+++ install.sh
@@ -1157,10 +1157,16 @@ function _install_you_complete_me() {
         return 1
     }
 
+    pushd "${HOME}/.vim/plugged/YouCompleteMe" || {
+        logger_err "Failed to change directry \"${HOME}/.vim/plugged/YouCompleteMe\""
+        return 1
+    }
     python3 install.py --clang-completer --system-libclang || {
+        popd
         logger_err "Failed to install with python3 install.py"
         return 1
     }
+    popd
 
     return 0
 }
