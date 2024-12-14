ed37aef59807a4593f699cfa8f8b3aeabbea47fb
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2024-12-14 12:59:54 +0900

    #194 Updated version of bats

--- test/helpers.bash
+++ test/helpers.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-TARGET="$2"
 BATS_TEST_SKIPPED=
+TARGET=${TARGET:-install.sh}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [[ ! -f "${SCRIPT_DIR}/lib/stub4bats.sh/stub.sh" ]] && {
     echo "ERROR: File \"${SCRIPT_DIR}/lib/stub4bats.sh/stub.sh\" was not existed"
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 }
 . "${SCRIPT_DIR}/lib/stub4bats.sh/stub.sh"
 
-[[ ! -f "${SCRIPT_DIR}/lib/stub4bats.sh/stub.sh" ]] && {
+[[ ! -f ""${SCRIPT_DIR}/../${TARGET}"" ]] && {
     echo "ERROR: File \"${SCRIPT_DIR}/../${TARGET}\" was not existed"
     exit 1
 }
